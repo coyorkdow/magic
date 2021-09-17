fbc1529e765bc56b2dacc91ce2f40860bf6b8090
coyorkdow <coyorkdow@outlook.com>
2021-09-17 21:27:09 +0800

    format code

--- quick_power.h
+++ quick_power.h
@@ -9,41 +9,35 @@
 
 namespace magic {
 
-template<uint64_t Num>
-struct MaxDigit {
-  static const uint64_t result;
-};
-template<uint64_t Num> const uint64_t MaxDigit<Num>::result = 1 + MaxDigit<(Num >> 1)>::result;
-template<> const uint64_t MaxDigit<0>::result = 0;
+template <uint64_t Num> struct MaxDigit { static const uint64_t result; };
+template <uint64_t Num>
+const uint64_t MaxDigit<Num>::result = 1 + MaxDigit<(Num >> 1)>::result;
+template <> const uint64_t MaxDigit<0>::result = 0;
 
-template<uint64_t Base, uint64_t EXP, uint64_t MOD>
-class Power {
+template <uint64_t Base, uint64_t EXP, uint64_t MOD> class Power {
 
-  template<uint64_t A, uint64_t B>
-  struct Times {
+  template <uint64_t A, uint64_t B> struct Times {
     static const uint64_t result = A * B % MOD;
   };
 
-  template<uint64_t DIGIT, uint64_t Current>
-  struct Process {
+  template <uint64_t DIGIT, uint64_t Current> struct Process {
     using Next = Process<DIGIT << 1, Current * Current % MOD>;
     static const uint64_t result =
-        IfThenElse<bool(DIGIT & EXP),
-                   Times<Current, Next::result>,
+        IfThenElse<bool(DIGIT &EXP), Times<Current, Next::result>,
                    Times<1, Next::result>>::Result::result;
   };
 
-  template<uint64_t Current>
+  template <uint64_t Current>
   struct Process<(1 << (MaxDigit<EXP>::result - 1)), Current> {
     static const uint64_t result = Current % MOD;
   };
 
- public:
+public:
   inline constexpr static uint64_t Calculate() {
     return Process<1, Base>::result;
   }
 };
 
-}
+} // namespace magic
 
 #endif // MAGIC__QUICK_POWER_H_
--- util.h
+++ util.h
@@ -7,19 +7,16 @@
 
 namespace magic {
 
-template<bool Cond, class Then, class Else>
-struct IfThenElse;
+template <bool Cond, class Then, class Else> struct IfThenElse;
 
-template<class Then, class Else>
-struct IfThenElse<true, Then, Else> {
+template <class Then, class Else> struct IfThenElse<true, Then, Else> {
   using Result = Then;
 };
 
-template<class Then, class Else>
-struct IfThenElse<false, Then, Else> {
+template <class Then, class Else> struct IfThenElse<false, Then, Else> {
   using Result = Else;
 };
 
-}
+} // namespace magic
 
 #endif // MAGIC__UTIL_H_
