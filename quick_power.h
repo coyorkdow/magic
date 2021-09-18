bc94f34b654dff8315d46dab1164708b085c5a27
coyorkdow <coyorkdow@outlook.com>
2021-09-18 16:37:54 +0800

    refine code style

--- quick_power.h
+++ quick_power.h
@@ -9,30 +9,36 @@
 
 namespace magic {
 
-template <uint64_t Num> struct MaxDigit { static const uint64_t result; };
-template <uint64_t Num>
+template<uint64_t Num>
+struct MaxDigit { static const uint64_t result; };
+
+template<uint64_t Num>
 const uint64_t MaxDigit<Num>::result = 1 + MaxDigit<(Num >> 1)>::result;
-template <> const uint64_t MaxDigit<0>::result = 0;
 
-template <uint64_t Base, uint64_t EXP, uint64_t MOD> class Power {
+template<> const uint64_t MaxDigit<0>::result = 0;
 
-  template <uint64_t A, uint64_t B> struct Times {
-    static const uint64_t result = A * B % MOD;
+template<uint64_t Base, uint64_t Exp, uint64_t Mod>
+class Power {
+
+  template<uint64_t A, uint64_t B>
+  struct Times {
+    static const uint64_t result = A * B % Mod;
   };
 
-  template <uint64_t DIGIT, uint64_t Current> struct Process {
-    using Next = Process<DIGIT << 1, Current * Current % MOD>;
+  template<uint64_t DIGIT, uint64_t Current>
+  struct Process {
+    using next = Process<DIGIT << 1, Current * Current % Mod>;
     static const uint64_t result =
-        IfThenElse<bool(DIGIT &EXP), Times<Current, Next::result>,
-                   Times<1, Next::result>>::Result::result;
+        IfThenElse<bool(DIGIT & Exp), Times<Current, next::result>,
+                   Times<1, next::result>>::result::result;
   };
 
-  template <uint64_t Current>
-  struct Process<(1 << (MaxDigit<EXP>::result - 1)), Current> {
-    static const uint64_t result = Current % MOD;
+  template<uint64_t Current>
+  struct Process<(1 << (MaxDigit<Exp>::result - 1)), Current> {
+    static const uint64_t result = Current % Mod;
   };
 
-public:
+ public:
   inline constexpr static uint64_t Calculate() {
     return Process<1, Base>::result;
   }
--- tuple.h
+++ tuple.h
@@ -9,54 +9,76 @@
 
 namespace magic {
 
-template <size_t Index, typename Tp> struct TupleComponent {
+template<size_t Index, class Tp>
+struct TupleComponent {
   Tp value;
   TupleComponent() = default;
   explicit TupleComponent(Tp v) : value(v) {}
 };
 
-template <class Index, typename... Tp> struct TupleImpl;
+template<class Index, class ...Tp>
+struct TupleImpl;
 
-template <size_t... IndexSeq, typename... Tp>
+template<size_t ...IndexSeq, class ...Tp>
 class TupleImpl<IndexSequence<IndexSeq...>, Tp...>
     : public TupleComponent<IndexSeq, Tp>... {
 
-  template <size_t Begin, size_t Index, typename Tp_, typename... Tps_>
+  template<size_t Begin, size_t Index, class Tp_, class ...Tps_>
   struct IterateElementType {
     static_assert(Index < sizeof...(IndexSeq), "index out of bound");
-    using result =
-        typename IterateElementType<Begin + 1, Index, Tps_...>::result;
+    using result = typename IterateElementType<Begin + 1, Index, Tps_...>::result;
   };
 
-  template <size_t Index, typename Tp_, typename... Tps_>
+  template<size_t Index, class Tp_, class ...Tps_>
   struct IterateElementType<Index, Index, Tp_, Tps_...> {
     using result = Tp_;
   };
 
-  template <size_t Index> struct GetElementType {
+  template<size_t Index>
+  struct GetElementType {
     using result = typename IterateElementType<0, Index, Tp...>::result;
   };
 
-public:
+ public:
   TupleImpl() = default;
 
-  template <typename... Up>
-  explicit TupleImpl(Up... up) : TupleComponent<IndexSeq, Tp>(up)... {}
+  template<class ...Up>
+  explicit TupleImpl(Up ...up) : TupleComponent<IndexSeq, Tp>(up)... {}
 
-  template <size_t Index> typename GetElementType<Index>::result &Get() {
+  template<size_t Index>
+  typename GetElementType<Index>::result &Get() {
     return TupleComponent<Index, typename GetElementType<Index>::result>::value;
   }
 
-  template <class Fn> void ForEach(Fn &&func) {
+  template<class Fn>
+  void ForEach(Fn &&func) {
     using expander = int[];
-    (void)expander{0, (void(func(Get<IndexSeq>())), 0)...};
+    (void) expander{0, (void(func(Get<IndexSeq>())), 0)...};
   }
 };
 
-template <typename... Tp>
-using Tuple = TupleImpl<MakeIndexSequence<sizeof...(Tp)>, Tp...>;
+template<class ...Tp>
+class Tuple : public TupleImpl<typename IndexSeqImpl<0, sizeof...(Tp) - 1>::result, Tp...> {
+ public:
+  Tuple() = default;
 
-template <class... Tp>
+  template<class ...Up>
+  explicit Tuple(Up ...up) :
+      TupleImpl<typename IndexSeqImpl<0, sizeof...(Tp) - 1>::result, Tp...>(up...) {}
+};
+
+template<class Tp>
+struct TupleSize;
+
+template<class ...Tp>
+struct TupleSize<Tuple<Tp...>> {
+  static const size_t result;
+};
+
+template<class ...Tp>
+const size_t TupleSize<Tuple<Tp...>>::result = sizeof...(Tp);
+
+template<class ...Tp>
 inline Tuple<typename std::decay<Tp>::type...> MakeTuple(Tp &&...t) {
   return Tuple<typename std::decay<Tp>::type...>(std::forward<Tp>(t)...);
 }
