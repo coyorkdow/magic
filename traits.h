c598afbb714457147a8e413eb7e409f24fbc0086
coyorkdow <coyorkdow@outlook.com>
2021-12-30 22:19:35 +0800

    add IsConstructible

--- traits.h
+++ traits.h
@@ -11,23 +11,30 @@ namespace magic {
 
 namespace detail {
 using std::begin;
+using std::declval;
 using std::end;
 
 template<class T, class = void>
 struct BeginType;
 
 template<class T>
-struct BeginType<T, decltype(begin(std::declval<T>()), ++begin(std::declval<T>()), (void) *begin(std::declval<T>()))> {
-  using type = decltype(begin(std::declval<T>()));
+struct BeginType<T, decltype(begin(declval<T>()), ++begin(declval<T>()), (void) *begin(declval<T>()))> {
+  using type = decltype(begin(declval<T>()));
 };
 
 template<class T, class = void>
 struct EndType;
 
 template<class T>
-struct EndType<T, decltype(end(std::declval<T>()), ++end(std::declval<T>()), (void) *end(std::declval<T>()))> {
-  using type = decltype(begin(std::declval<T>()));
+struct EndType<T, decltype(end(declval<T>()), ++end(declval<T>()), (void) *end(declval<T>()))> {
+  using type = decltype(begin(declval<T>()));
 };
+
+template<class T, class... Args>
+auto IsConstructibleImpl(int) -> decltype(T(declval<Args&>()...), std::true_type{});
+
+template<class T, class...>
+auto IsConstructibleImpl(...) -> std::false_type;
 
 }// namespace detail
 
@@ -40,6 +47,9 @@ struct IsIterable<T,
                                                        typename detail::EndType<T>::type>{},
                                           void>::type>
     : public std::true_type {};
+
+template<class T, class... Args>
+struct IsConstructible : public std::integral_constant<bool, decltype(detail::IsConstructibleImpl<T, Args...>(0))::value> {};
 
 }// namespace magic
 
